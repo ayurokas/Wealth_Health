916821f02c25b2497f8cdc6a10c01dea013a578b
perso <elisa.github@gmail.com>
2023-12-26 18:34:31 +0100

    maj

--- projet_react/src/Pages/Home.jsx
+++ projet_react/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Form from "../Composant/CreateEmploye";
+import React, { Suspense } from 'react';
+import { Link } from 'react-router-dom';
+const Form = React.lazy(() => import('../Composant/CreateEmploye')); // Lazy-load the Form component
 
 export default function CreateEmployee() {
   return (
@@ -9,13 +9,15 @@ export default function CreateEmployee() {
         <div className="title">
           <h1>WEALTH HEALTH</h1>
         </div>
-        <Link to="/listEmployee" className="view-employees-link">View Current Employees</Link>      
+        <Link to="/listEmployee" className="view-employees-link">View Current Employees</Link>
       </header>
       <main>
         <div className="container">
           <h2>HRnet</h2>
           <h3>Create Employee</h3>
-          <Form />
+          <Suspense fallback={<div>Loading...</div>}> {/* Add Suspense for lazy-loaded components */}
+            <Form />
+          </Suspense>
         </div>
       </main>
     </>
